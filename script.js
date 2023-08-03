8c37758204e8f4417cb0e945bc8219c47a408b44
anilcme <74306004+anilcme@users.noreply.github.com>
2023-08-02 23:01:36 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-document.getElementById("tempConverter").addEventListener("submit", function (e) {
-    e.preventDefault();
+function showConfirmationPopup() {
+      var savePassword = confirm("Do you want to save your password?");
+      if (savePassword) {
+        alert("Password saved!");
+      } else {
+        alert("Password not saved.");
+      }
 
-    const temperature = parseFloat(document.getElementById("temperature").value);
-    const unit = document.getElementById("unit").value;
-
-    let result;
-
-    if (unit === "celsius") {
-        result = celsiusToFahrenheit(temperature);
-    } else {
-        result = fahrenheitToCelsius(temperature);
+      alert("Email registration confirmed!");
     }
-
-    document.getElementById("result").innerText = `Result: ${result.toFixed(2)} °${unit.toUpperCase()}`;
-});
-
-function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-}
-
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
